c0a430f10e4dd95d63d92ce400d3d6feac3b5405
ElonTusk404 <igmaflappy@gmail.com>
2023-07-04 20:04:46 +0400

    Update is_sorted_function.py

--- is_sorted_function.py
+++ is_sorted_function.py
@@ -1,5 +1,5 @@
 def is_sorted(onelist):
-    for i in range(1, len(onelist)):
-        if onelist[i] < onelist[i+1]:
+    for i in range(len(onelist)-1):
+        if onelist[i] > onelist[i+1]:
             return False
     return True
